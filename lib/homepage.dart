6332e9fa8709693eeea07b9c2c5877afc4ca5860
Debjit Chakraborty <zxyron0@gmail.com>
2020-08-27 21:53:12 +0530

    Improve visuals

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,32 +18,35 @@ class _HomePageState extends State<HomePage> {
 
   getButton() {
     return _controller.text == ''
-        ? MaterialButton(
-            minWidth: 180,
-            height: 60,
-            child: Text(
-              'Generate',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
+        ? Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: MaterialButton(
+              minWidth: 180,
+              height: 60,
+              child: Text(
+                'Generate',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
+              color: Colors.blue,
+              onPressed: () async {
+                if (!uppercase && !lowercase && !specialchars && !numbers) {
+                  _scaffoldkey.currentState.removeCurrentSnackBar();
+                  _scaffoldkey.currentState.showSnackBar(SnackBar(
+                    content: Text('Select atleast one value'),
+                    duration: Duration(milliseconds: 500),
+                  ));
+                } else {
+                  Generator generator = new Generator();
+                  await generator.addFunctions();
+                  setState(() {
+                    _controller.text = generator.generate(_sliderValue.toInt());
+                  });
+                }
+              },
             ),
-            color: Colors.blue,
-            onPressed: () async {
-              if (!uppercase && !lowercase && !specialchars && !numbers) {
-                _scaffoldkey.currentState.removeCurrentSnackBar();
-                _scaffoldkey.currentState.showSnackBar(SnackBar(
-                  content: Text('Select atleast one value'),
-                  duration: Duration(milliseconds: 500),
-                ));
-              } else {
-                Generator generator = new Generator();
-                await generator.addFunctions();
-                setState(() {
-                  _controller.text = generator.generate(_sliderValue.toInt());
-                });
-              }
-            },
           )
         : Padding(
             padding: const EdgeInsets.symmetric(horizontal: 30.0),
@@ -88,10 +91,22 @@ class _HomePageState extends State<HomePage> {
       key: _scaffoldkey,
       appBar: AppBar(
         title: Text('Password Manager'),
+        centerTitle: true,
       ),
       body: Center(
-        child: Column(
+        child: ListView(
           children: <Widget>[
+            ListTile(
+              contentPadding: EdgeInsets.all(10.0),
+              dense: true,
+              title: Text(
+                'Generator',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
             ListTile(
               contentPadding: EdgeInsets.symmetric(
                 horizontal: 20.0,
@@ -161,9 +176,16 @@ class _HomePageState extends State<HomePage> {
                 horizontal: 20.0,
                 vertical: 5.0,
               ),
-              title: Text('Password Length: '),
+              title: Row(
+                children: [
+                  Text('Password Length: '),
+                  Spacer(),
+                  Text(_sliderValue.toInt().toString()),
+                  SizedBox(width: 15.0)
+                ],
+              ),
               subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                padding: const EdgeInsets.only(top: 20.0),
                 child: Row(
                   children: [
                     IconButton(
@@ -200,14 +222,6 @@ class _HomePageState extends State<HomePage> {
                       },
                     ),
                   ],
-                ),
-              ),
-            ),
-            Center(
-              child: Text(
-                _sliderValue.toInt().toString(),
-                style: TextStyle(
-                  fontSize: 18.0,
                 ),
               ),
             ),
