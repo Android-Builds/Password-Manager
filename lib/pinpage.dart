4cd243b0827276c32f5ed1293c7a73318e11c451
Debjit Chakraborty <zxyron0@gmail.com>
2020-08-29 22:51:56 +0530

    Add controllers to enter and erase pin

--- lib/pinpage.dart
+++ lib/pinpage.dart
@@ -7,26 +7,69 @@ class PinPage extends StatefulWidget {
 }
 
 class _PinPageState extends State<PinPage> {
-  TextEditingController _controller = new TextEditingController();
+  String _password = '';
+  List<FocusNode> nodes = List<FocusNode>();
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 6; i++) {
+      nodes.add(new FocusNode());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    double width = 55.0;
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            width: width,
-            padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 30.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                fillColor: darkmode ? Colors.grey[800] : Colors.grey[350],
-                filled: true,
-              ),
-            ),
+            height: 200,
+            child: ListView.builder(
+                padding: EdgeInsets.all(20.0),
+                itemCount: 6,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Center(
+                    child: Container(
+                      height: 60.0,
+                      width: 55.0,
+                      padding: EdgeInsets.symmetric(horizontal: 5.0),
+                      child: TextField(
+                        controller: new TextEditingController(),
+                        maxLength: 1,
+                        focusNode: nodes[index],
+                        autofocus: true,
+                        showCursor: true,
+                        onChanged: (value) {
+                          _password += value;
+                          if (value == '') {
+                            nodes[index].previousFocus();
+                          } else {
+                            nodes[index].nextFocus();
+                          }
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          fillColor:
+                              darkmode ? Colors.grey[800] : Colors.grey[350],
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
           ),
+          MaterialButton(
+              child: Text('Login'),
+              minWidth: 120,
+              height: 50,
+              color: Colors.blue,
+              onPressed: () {
+                print(_password);
+              })
         ],
       ),
     );
